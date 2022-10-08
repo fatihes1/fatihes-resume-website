77e80d3cd4ad1fd561dc2cb2af9fc26c25c1c2d2
fatihes1 <18360859021@ogrenci.btu.edu.tr>
2022-10-08 23:33:58 +0300

    Added avatar pictures

--- resume-website/src/components/SkilllsSection/CoverSection.vue
+++ resume-website/src/components/SkilllsSection/CoverSection.vue
@@ -1,10 +1,16 @@
 <template>
-  <div
-    class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
-  >
-    <div class="flex flex-col mb-6 lg:flex-row md:mb-10">
-      <div class="lg:w-1/2">
-        <img src="@/assets/images/avatar1.png" />
+  <div class="mx-auto max-w-screen-xl">
+    <div class="flex flex-col">
+      <div class="grid grid-cols-3 row-gap-10">
+        <div class="flex flex-row justify-center items-center">
+          <img src="@/assets/images/avatar1.png" class="w-40 h-40" />
+        </div>
+        <div class="flex flex-row justify-center items-center">
+          <img src="@/assets/images/avatar2.png" class="w-80 h-80" />
+        </div>
+        <div class="flex flex-row justify-center items-center">
+          <img src="@/assets/images/avatar3.png" class="w-40 h-40" />
+        </div>
       </div>
       <!-- <div class="lg:w-1/2">
         <p class="text-base text-gray-700 md:text-lg">
@@ -14,7 +20,7 @@
         </p>
       </div> -->
     </div>
-    <div class="grid gap-8 row-gap-10 sm:grid-cols-2 lg:grid-cols-4">
+    <div class="grid gap-8 row-gap-10 grid-cols-4">
       <SkillArea
         v-for="informaton in SkillsInformation"
         :key="id"
