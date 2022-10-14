1313c737b98a35bd8fbfdbe197304f8e967976e5
fatihes1 <18360859021@ogrenci.btu.edu.tr>
2022-10-14 17:46:10 +0300

    Added avatar swap animation

--- resume-website/src/components/SkilllsSection/CoverSection.vue
+++ resume-website/src/components/SkilllsSection/CoverSection.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="mx-auto max-w-screen-xl">
-    <div class="flex flex-col">
-      <div class="grid grid-cols-3 row-gap-10">
-        <div class="flex flex-row justify-center items-center">
-          <img src="@/assets/images/avatar1.png" class="w-40 h-40" />
-        </div>
-        <div class="flex flex-row justify-center items-center">
-          <img src="@/assets/images/avatar2.png" class="w-80 h-80" />
-        </div>
-        <div class="flex flex-row justify-center items-center">
-          <img src="@/assets/images/avatar3.png" class="w-40 h-40" />
-        </div>
+    <div class="flex flex-row">
+      <div class="w-1/3 flex flex-col items-center gap-y-10 mt-14">
+        <SkillArea :skillInfo="SkillsInformation[0]" />
+        <SkillArea :skillInfo="SkillsInformation[1]" />
       </div>
-      <!-- <div class="lg:w-1/2">
-        <p class="text-base text-gray-700 md:text-lg">
-          For those who have seen the Earth from space, and for the hundreds and
-          perhaps thousands more who will, the experience most certainly changes
-          your perspective.
-        </p>
-      </div> -->
-    </div>
-    <div class="grid gap-8 row-gap-10 grid-cols-4">
-      <SkillArea
-        v-for="informaton in SkillsInformation"
-        :key="id"
-        :skillInfo="informaton"
-      />
-      <!-- <SkillArea :skillInfo="info" />
-      <SkillArea :skillInfo="info" />
-      <SkillArea :skillInfo="info" /> -->
+      <div class="w-1/3 flex flex-col gap-x-5 justify-center items-center">
+        <label class="swap swap-flip text-9xl">
+          <!-- this hidden checkbox controls the state -->
+          <input type="checkbox" />
+
+          <div class="swap-on">
+            <img src="@/assets/images/avatar1.png" class="w-96 h-96" />
+          </div>
+          <div class="swap-off">
+            <img src="@/assets/images/avatar2.png" class="w-96 h-96" />
+          </div>
+        </label>
+
+        <h2 class="align-middle">
+          <img
+            :src="`src/assets/images/social/github.svg`"
+            class="w-10 h-10 inline mr-2"
+          />
+          <a
+            href="https://github.com/fatihes1"
+            target="_blank"
+            class="inline text-3xl code_title align-middle hover:text-amber-600 transition-colors"
+            >@fatihes1</a
+          >
+        </h2>
+      </div>
+      <div class="w-1/3 flex flex-col items-center gap-y-10 mt-14">
+        <SkillArea class="items-end" :skillInfo="SkillsInformation[3]" />
+        <SkillArea :skillInfo="SkillsInformation[2]" />
+      </div>
     </div>
   </div>
 </template>
@@ -45,8 +51,14 @@ export default {
       SkillsInformation,
     };
   },
-  mounted() {
-    console.log(this.SkillsInformation);
-  },
 };
 </script>
+<!-- <div class="flex flex-row justify-center items-center">
+          <img src="@/assets/images/avatar2.png" class="w-80 h-80" />
+        </div> -->
+<!-- 
+        <SkillArea
+        v-for="informaton in SkillsInformation"
+        :key="id"
+        :skillInfo="informaton"
+      /> -->
